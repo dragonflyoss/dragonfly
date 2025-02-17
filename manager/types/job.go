abce59c87718e414b4bf3940ed297f7eba2083d3
southwest miao <1403572259@qq.com>
2025-02-17 13:03:13 +0800

    feat: add load_to_cache field in preheat request
    Signed-off-by: southwest miao <1403572259@qq.com>

--- manager/types/job.go
+++ manager/types/job.go
@@ -137,6 +137,9 @@ type PreheatArgs struct {
 
 	// Timeout is the timeout for preheating, default is 30 minutes.
 	Timeout time.Duration `json:"timeout" binding:"omitempty"`
+
+	// LoadToCache is the flag for preheating content in cache storage.
+	LoadToCache bool `json:"load_to_cache" binding:"omitempty"`
 }
 
 type CreateSyncPeersJobRequest struct {
