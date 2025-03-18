128f2d9a324f6879e21cd564668ac231493e7262
BruceAko <chongzhi@hust.edu.cn>
2025-03-18 13:12:09 +0800

    feat: add persistent cache service
    Signed-off-by: BruceAko <chongzhi@hust.edu.cn>

---
+++ manager/handlers/persistent_cache.go
@@ -0,0 +1,55 @@
+/*
+ *     Copyright 2025 The Dragonfly Authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package handlers
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+
+	"d7y.io/dragonfly/v2/manager/types"
+)
+
+// @Summary Get PersistentCaches
+// @Description Get PersistentCaches
+// @Tags PersistentCache
+// @Accept json
+// @Produce json
+// @Param page query int true "current page" default(0)
+// @Param per_page query int true "return max item count, default 10, max 50" default(10) minimum(2) maximum(50)
+// @Success 200 {object} []types.GetPersistentCachesResponse
+// @Failure 400
+// @Failure 404
+// @Failure 500
+// @Router /api/v1/persistent-caches [get]
+func (h *Handlers) GetPersistentCaches(ctx *gin.Context) {
+	var query types.GetPersistentCachesQuery
+	if err := ctx.ShouldBindQuery(&query); err != nil {
+		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"errors": err.Error()})
+		return
+	}
+
+	h.setPaginationDefault(&query.Page, &query.PerPage)
+	persistentCaches, count, err := h.service.GetPersistentCaches(ctx.Request.Context(), query)
+	if err != nil {
+		ctx.Error(err) // nolint: errcheck
+		return
+	}
+
+	h.setPaginationLinkHeader(ctx, query.Page, query.PerPage, int(count))
+	ctx.JSON(http.StatusOK, persistentCaches)
+}
