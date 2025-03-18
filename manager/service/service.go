fc4314a730e7cc78d105b7ced491e471a2dfc9f8
BruceAko <chongzhi@hust.edu.cn>
2025-03-18 13:12:09 +0800

    feat: add persistent cache service
    Signed-off-by: BruceAko <chongzhi@hust.edu.cn>

--- manager/service/service.go
+++ manager/service/service.go
@@ -137,6 +137,8 @@ type Service interface {
 	UpdatePersonalAccessToken(context.Context, uint, types.UpdatePersonalAccessTokenRequest) (*models.PersonalAccessToken, error)
 	GetPersonalAccessToken(context.Context, uint) (*models.PersonalAccessToken, error)
 	GetPersonalAccessTokens(context.Context, types.GetPersonalAccessTokensQuery) ([]models.PersonalAccessToken, int64, error)
+
+	GetPersistentCaches(context.Context, types.GetPersistentCachesQuery) ([]types.GetPersistentCacheResponse, int64, error)
 }
 
 type service struct {
